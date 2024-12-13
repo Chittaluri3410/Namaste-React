05752e831ea32c1da4d8bf1d37d66ffbec486753
Chittaluri3410 <chittalurisaivardhan@gmail.com>
2024-12-13 20:31:56 +0530

    Import react through npm,module,

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const heading = React.createElement("h1", {}, "Hello world from React!"); // Creates an <h1> React element with the text "Hello world from React!"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Finds the <div> with id "root" and prepares it for rendering React components 
-//ReactDOM.createRoot is a function provided by React 18 that creates a root to manage the rendering of your React components.
+// Creates an <h1> React element with the text "Hello world from React!"
+const heading = React.createElement("h1", {}, "Hello world from React!");
 
-root.render(heading); // Renders the 'heading' element inside the 'root' div
+// Finds the <div> with id "root" and prepares it for rendering React components 
+// ReactDOM.createRoot is a function provided by React 18 that creates a root to manage the rendering of your React components.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Renders the 'heading' element inside the 'root' div
+root.render(heading);
