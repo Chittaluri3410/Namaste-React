092b4855989098c9bc6e72305e62244c093c2d43
Chittaluri3410 <chittalurisaivardhan@gmail.com>
2024-12-31 18:26:09 +0530

    clean 5.1

--- Desktop/original Namaste React/episode 5/episode 5.1/ep 5.1/src/components/Body.js
+++ Desktop/original Namaste React/episode 5/episode 5.1/ep 5.1/src/components/Body.js
@@ -6,7 +6,10 @@ import resList from "../utils/mockData";
 const Body = () => {
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button className="filter-btn" onClick={() => {alert("you clicked")}}>
+            Top Rated Restaurant</button>  
+        </div>
         <div className="res-container">
           {resList.map((restaurant) => (
             <RestaurantCard key={restaurant.info.id} resObj={restaurant.info} />
